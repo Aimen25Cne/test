f9aa7999a323183b6bb8fdbe323a2163e0a78e41
Aimen25Cne <aimen25cne@gmail.com>
2021-07-15 17:52:38 +0100

    Add files via upload

---
+++ About-Us.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="Online tools provides md2, md5, sha1, sha2, sha512, bas64, html encode / decode functions">
+
+
+    <title>About Us
+    </title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
+  </head>
+  <body>
+    <div id="header">
+      <h1><a href="index.html">Online Tools</a></h1>
+    </div>
+    <div id="body">
+
+<h1 class="mb-1 mt-3 text-center font-weight-medium">About Us</h1>
+<p class="mb-1 mt-2 mr-3 ml-3 text-center font-weight-light">showtube.ml is a free online tools provider.</p>
+<p>showtube.ml can help you to enjoy your favorite YouTube videos with friends and family members</p>
+<p><strong>Features of showtube.ml:</strong></p>
+<ul>
+<li>Fully free tools.</li>
+<li>Web app available.</li>
+<li>Directly send feedback to developers.</li>
+</ul>
+<p>&nbsp;</p>
+<p>We are constantly adding new tools and improving previous available tools</p>
+<p class="body-1">If you are receiving value from our free online tools services, we welcome your support.&nbsp;</p>
+
+<div id="footer"><a title="Home" href="https://showtube.ml/">Home</a>&nbsp; <a title="About Us" href="./About-Us.php">About Us</a>&nbsp; <a title="Privacy Policy " href="./privacy-policy.php">Privacy Policy </a>&nbsp; <a title="Disclaimer" href="./Disclaimer.php">Disclaimer</a> <a title="feedback" href="./feedback.php">feedback</a> <a title="Contact Us" href="./contact.php">Contact Us</a> &nbsp;&copy; 2015-2021 Online Tools</div>  </body>
+</html>
